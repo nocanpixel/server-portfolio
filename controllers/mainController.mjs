12e3964d79267a372a4dba134709671fbc610278
nocanpixel <54224292+nocanpixel@users.noreply.github.com>
2023-09-22 22:41:17 +0200

    Headers

--- controllers/mainController.mjs
+++ controllers/mainController.mjs
@@ -1,12 +1,15 @@
 class mainController {
   async welcome(req, res) {
+    res.setHeader('Cache-Control', 'no-cache, no-store, must-revalidate');
+    res.setHeader('Pragma', 'no-cache');
+    next();
     // You can access Morgan data from the `req` object
     const clientIpAddress = req.ip; // IP address of the client
     const httpMethod = req.method; // HTTP method (GET, POST, etc.)
     const url = req.originalUrl; // Requested URL
     const statusCode = res.statusCode; // Response status code
     const responseTime = res.getHeader("X-Response-Time"); // Response time in milliseconds
-    
+
     res.json(
      {
         response: {
